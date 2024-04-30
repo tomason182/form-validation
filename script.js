c5becfbd14df407dadfc08b462d822b1fd0b369e
tomason182 <elotro182@hotmail.com>
2024-04-30 12:18:29 -0300

    style valid invalid psw check

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const email = document.getElementById("mail");
 const emailError = document.querySelector("span.email-error");
 const psw = document.getElementById("psw");
 const pswError = document.querySelector("span.psw-error");
-console.log(pswError);
 
 
 email.value = "";
@@ -10,7 +9,7 @@ email.value = "";
 function showEmailError() {
     if (email.validity.valueMissing) {
         emailError.textContent = "You need to enter an email address";
-        email.className("");
+        email.className = "";
     } else if (email.validity.typeMisMatch) {
         emailError.textContent = "You need to enter a valid email address";
         email.classList.add("invalid");
@@ -36,9 +35,12 @@ psw.addEventListener("focusout", (event) => {
 function checkPassword(password) {
     const pswRegExp = /^(?=.*\d)(?=.*[A-Z])(?=.*\W).{8,24}$/;
     const isValid = pswRegExp.test(password);
+
+    psw.className = "";
   
     if (isValid) {
       pswError.textContent = "";
+      psw.className = "valid";
     } else {
       let message = "Password must contain: ";
       if (!/\d/.test(password)) message += "at least one number ";
@@ -46,6 +48,7 @@ function checkPassword(password) {
       if (!/\W/.test(password)) message += "at least one special character ";
       if (password.length <= 8 || password.length >= 24) message += "between 8 and 24 characters";
       pswError.textContent = message;
+      psw.className = "invalid";
     }
   }
 
